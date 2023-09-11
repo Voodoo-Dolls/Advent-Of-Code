71adc4c216c2e4b5193e8675139d13aad012617b
Michael <michaeldam.dev@gmail.com>
2023-09-11 13:11:20 -0600

    Ask Daniel about the RegEx

--- Day6.js
+++ Day6.js
@@ -4,8 +4,8 @@ let findMarker =(signal)=>{
     let regEx = /(.).*\1/;
     for (let i = 0; i < signal.length; i++){
 
-        if (!regEx.test(signal.slice(i,i+4))){
-            console.log(i + 4);
+        if (!regEx.test(signal.slice(i,i+14))){
+            console.log(i + 14);
             // console.log(signal.slice(i,i + 4))
             break;
         }
