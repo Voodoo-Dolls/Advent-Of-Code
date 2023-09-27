ced1b5a8c9ad495604d41f687c8d5d06d4b4727d
Michael Dam <92550503+Voodoo-Dolls@users.noreply.github.com>
2023-09-26 22:21:06 -0600

    updated framework

--- Day11.js
+++ Day11.js
@@ -1,19 +1,25 @@
-let addOperation = (operator) => {
+// Part of Monkey Constructor
+let addOperation = (operator, value) => {
+  if (value === null) {
+    return (input) => {
+      return input * input;
+    };
+  }
   if (operator === "+") {
-    return (value, input) => {
+    return (input) => {
       return value + input;
     };
   }
   if (operator === "*") {
-    return (value, input) => {
+    return (input) => {
       return value * input;
     };
   }
 };
-
-function Monkey(items, operator, divisible, monkeyA, monkeyB) {
+// Monkey Constructor
+function Monkey(items, operator, opValue, divisible, monkeyA, monkeyB) {
   this.items = [...items];
-  this.operation = addOperation(operator);
+  this.operation = addOperation(operator, opValue);
   this.test = (input) => {
     if (input % divisible === 0) {
       return monkeyA;
@@ -23,4 +29,13 @@ function Monkey(items, operator, divisible, monkeyA, monkeyB) {
   };
 }
 
-let monkey0 = new Monkey([79, 98], "*", 23, 2, 3);
+// Monkey Objects
+let monkey0 = new Monkey([79, 98], "*", 19, 23, 2, 3);
+let monkey1 = new Monkey([54, 65, 75, 74], "+", 6, 19, 2, 0);
+let monkey2 = new Monkey([79, 60, 97], "*", null, 13, 1, 3);
+let monkey3 = new Monkey([74], "+", 3, 17, 0, 1);
+
+// Monkey Objects Placed in Array
+let monkeyArray = [monkey0, monkey1, monkey3];
+
+// Round Simulator
